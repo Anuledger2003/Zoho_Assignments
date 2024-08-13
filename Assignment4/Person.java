7f3176068f7d5727bf809fdaf15d5c50d5fff5d7
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 10:03:08 +0530

    Update Person.java

--- Assignment4/Person.java
+++ Assignment4/Person.java
@@ -1,8 +1,5 @@
 package assignment3;
 
-import java.io.*;
-import java.util.Scanner;
-
 public class Person {
 	String name;
 	int age ;
@@ -13,7 +10,6 @@ public class Person {
 	 * second takes a String argument for the name and an int argument for the age and 
 	 * third constructor takes a String argument for the name, an int argument for the age, and a String argument for the address.
 	 */
-	
 	
 	//First Constructor
 	Person(String name){  
@@ -33,27 +29,16 @@ public class Person {
 		this.address = address ;
 	}
 	
-	
 	public static void main(String[] args) {
-		Scanner obj = new Scanner(System.in);
-		
-		System.out.println("Enter the name : ");
-		String name = obj.nextLine();
-		
-		System.out.println("Enter the Address : ");
-		String address = obj.nextLine();
-		
-		System.out.println("Enter the Age : ");
-		int age = obj.nextInt();
 
-		Person n = new Person (name);
+		Person n = new Person ("ANU");
 		System.out.println("Name :" + n.name);
 		
-		Person a = new Person (name , age);
+		Person a = new Person ("ANU" , 21);
 		System.out.println("Name :" + a.name);
 		System.out.println("Age :" + a.age);
 		
-		Person ad = new Person (name , age , address);
+		Person ad = new Person ("ANU" , 21 , "PERAVURANI");
 		System.out.println("Name :" + ad.name);
 		System.out.println("Age :" + ad.age);
 		System.out.println("Address :" + ad.address);
