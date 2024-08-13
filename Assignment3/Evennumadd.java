95529e2ad75ea042dc066dcf8a2a5aed88c24895
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 09:42:39 +0530

    Update Evennumadd.java

--- Assignment3/Evennumadd.java
+++ Assignment3/Evennumadd.java
@@ -1,33 +1,17 @@
 package assignment2;
 
-import java.util.Scanner;
-
 public class Evennumadd {
 	public static void main(String[] args) {
-		Scanner obj = new Scanner(System.in);
 		
-		System.out.println("Enter the size of array ");
-		int num = obj.nextInt();
-		
-		System.out.println("Enter the elements ");
-		int array[] = new int[num];
-		for(int i = 0 ; i < num ; i++) {
-			array[i] = obj.nextInt();
-		}
-		
+		int[] array = {4, 2, 7, 2, 5, 9, 3, 8};
 		int even = 0 ;
 		
 		for (int i = 0; i < array.length; i++) {
 		            if (array[i] % 2 == 0) {
 		                even += array[i];
-		            }
-		            if(array[i] %2 != 0) {
-		            	continue ;
+				    if(array[i] %2 != 0) continue ;
 		            }
 		        }
-		 
-		        System.out.println("Even sum: " + even);
-		
+		System.out.println("Even sum: " + even);
 	}
-
 }
