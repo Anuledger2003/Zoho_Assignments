f9f0f28f478377cf0c4d45adcbcdb5b8e1bb0c39
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 09:16:35 +0530

    Update Secondocc.java

--- Assignment3/Secondocc.java
+++ Assignment3/Secondocc.java
@@ -4,35 +4,18 @@ import java.util.Scanner;
 
 public class Secondocc {
 	public static void main(String[] args) {
-		Scanner obj = new Scanner(System.in);
-		
-		System.out.println("Enter the number of elements ");
-		int num = obj.nextInt();
-		
-		System.out.println("Enter the array elements ");
-		int array[] = new int[num] ;
-		for(int i = 0 ; i < num ; i++) {
-			array[i] = obj .nextInt();
-		}
-		System.out.println("Enter the Key elements ");
-		int keyelement = obj.nextInt();
-		
-		int count = 0 ;
+		int[] array = {4, 2, 7, 2, 5, 9, 3, 8};
+
+		int keyelement = 2;	
+		int c = 0 ;
 		int i = 0;
 		for( i = 0 ;i < array.length ; i++) {
-			if(array[i] == keyelement) {
-				count = count + 1; ;
-			}
-			if(count == 2) {
-				break ;
+			if(array[i] == keyelement) {		
+				c++ ;
+				if(c == 2) break ;
 			}
 		}
-		if(count == 2) {
-			System.out.println("The second occurance of key element is located at "+ i );
-			
-		}
-		
+		if(c == 2)System.out.println("The second occurance of key element is located at "+ i );	
 	}
-
 }
 	
