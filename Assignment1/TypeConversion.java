19bd8cc82ec5d0f3e7ab821c4b8ada509875fac9
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 08:49:20 +0530

    Update TypeConversion.java

--- Assignment1/TypeConversion.java
+++ Assignment1/TypeConversion.java
@@ -3,24 +3,19 @@ package assignment1;
 public class TypeConversion {
 	public static void main(String[]args) {
 		
-		//Widening Casting
-		
 		int intnum = 4 ;
 		double doublenum = intnum ;
 		
 		System.out.println("The integer type value is\t"+intnum);
 		System.out.println("The Double type value is\t"+doublenum);
 		
-		//Narrowing Casting
-		
-		double num1 = 86.6745341267 ;// double upto 15 decimals
+		double num1 = 86.6745341267 ;
 		float num2 = (float)num1 ;
 		int num3 = (int)num2 ;
 		
 		System.out.println("The integer type value is\t"+num1);
 		System.out.println("The Double type value is\t"+num2);
-		System.out.println("The Double type value is\t"+num3);
-		
+		System.out.println("The Double type value is\t"+num3);		
 		
 	}
 
