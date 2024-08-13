425c911a52359a0b5eb8825fa41f0c962b79bcd3
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 08:56:40 +0530

    Update Bitwiseoperations.java

--- Assignment2/Bitwiseoperations.java
+++ Assignment2/Bitwiseoperations.java
@@ -13,16 +13,12 @@ public class Bitwiseoperations {
 		int bitand = firstnum & secondnum;
 		int bitor = firstnum | secondnum;
 		int bitxor = firstnum ^ secondnum;
-		int bitfirstnot = ~ firstnum ;
-		int bitsecondnot = ~ secondnum ;
 		int bitrightshift = firstnum >> 2 ;
-		int bitleftshift = secondnum >> 2 ;
+		int bitleftshift = secondnum << 2 ;
 		
 		System.out.println("AND OPERATION : " +bitand);
 		System.out.println("OR OPERATION : "+bitor);
 		System.out.println("XOR OPERATION : "+bitxor);
-		System.out.println("NOT FIRST NUMBER : "+bitfirstnot);
-		System.out.println("NOT SECOND NUMBER : "+bitsecondnot);
 		System.out.println("FIRST NUMBER'S RIGHT SHIFT : "+bitrightshift);
 		System.out.println("SECOND NUMBER'S LEFT SHIFT : "+bitleftshift);
 		
