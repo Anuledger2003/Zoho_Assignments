3567558b0e12f74bf5d27958340199d74abc14ee
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 10:13:22 +0530

    Update Main.java

--- Assignment4/Main.java
+++ Assignment4/Main.java
@@ -1,47 +1,46 @@
-package assignment3;
+package assignment4;
 
-import java.util.Scanner;
-
+/*Create a Main class with a main method to test the Person class by creating objects using each constructor and 
+ *printing their details.
+ */
 public class Main {
-	/*Create a Main class with a main method to test the Person class by creating objects using each constructor and 
-	 *printing their details.
-	 */
-	public static void main(String[] args) {
-		Scanner obj = new Scanner(System.in);
-		
-		System.out.println("Enter the name : ");
-		String name = obj.nextLine();
-		
-		System.out.println("Enter the Age : ");
-		int age = obj.nextInt();
-		
-		NewPerson person1 = new NewPerson (name , age);
-		System.out.println("Person 1 Information:");
-        person1.display();
-		
-	}
-}
+    public static void main(String[] args) {
 
-class NewPerson {
-			static String name;
-			static int age ;
-			
-			//First Constructor
-			NewPerson(String name){  
-				this.name = name;
-			}
-			
-			//Second Constructor
-			NewPerson(String name , int age){  
-				this.name = name;
-				this.age = age ;
-			}
-				
-			public static void display() {
-				System.out.println("Name :" + name);
-				System.out.println("Age :" + age);
-						
-			}
-		
-	}
+        Person person1 = new Person("ANU");
+        person1.printDetails();
+
+        Person person2 = new Person("ANU", 21);
+        person2.printDetails();
+
+        Person person3 = new Person("ANU", 21, "PERAVURANI");
+        person3.printDetails();
+    }
+}
+class Person {
+    String name;
+    int age;
+    String address;
+
+    public Person(String name) {
+        this.name = name;
+        this.age = 0;
+        this.address = "Not Available";
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.address = "Not Available";
+    }
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public void printDetails() {
+        System.out.println("Name: " + name + "\nAge: " + age + "\nAddress: " + address);
+    }
+}
 
