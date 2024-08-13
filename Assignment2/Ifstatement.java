37ba9223e6270fd23c831ad6cc7f3fa4bb3a6d12
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 08:51:58 +0530

    Update Ifstatement.java

--- Assignment2/Ifstatement.java
+++ Assignment2/Ifstatement.java
@@ -3,11 +3,9 @@ pacakge assignment1;
 class Ifstatement {
     public static void main(String[] args) {
         System.out.println("I am going to execute IF statement");
-        String outsidevar ;
-        outsidevar = "I am a Outside Variable " ;
+        String outsidevar = "I am a Outside Variable " ;
         if (true) {
-            String insidevar ;
-            insidevar = "I am a Inside Variable" ;
+            String insidevar ="I am a Inside Variable" ;
             System.out.println(insidevar);
         }
         System.out.println(outsidevar);
