80805d68cb3e3e5eb1aec3c193965ae9a13731bd
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 08:59:29 +0530

    Update CelsiustoFahrenheit.java

--- Assignment2/CelsiustoFahrenheit.java
+++ Assignment2/CelsiustoFahrenheit.java
@@ -9,9 +9,7 @@ public class CelsiustoFahrenheit {
 		System.out.println("Enter the Celsius ( C ): ") ;
 		float celsius = obj.nextFloat() ;
 		
-		float conversion ;
-		conversion = (celsius *9) / 5 ;
-		conversion += 32 ;
+		float conversion = (float)(((celsius *9) / 5)+32) ;
 		System.out.println("The Coversion from Celsius to Fahrenhit is " + conversion+"F") ;
 		
 	}
