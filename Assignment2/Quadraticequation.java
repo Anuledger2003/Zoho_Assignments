4959667d04b5dcae05349d2c9f3d6513e1efe86c
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 08:54:14 +0530

    Update Quadraticequation.java

--- Assignment2/Quadraticequation.java
+++ Assignment2/Quadraticequation.java
@@ -7,20 +7,20 @@ class Quadraticequation{
         System.out.println("Let's solve Quadratic Equation --> ax^2 + bx + c");
         Scanner obj = new Scanner(System.in);
         System.out.println("Enter the \"a\" value");
-        double a = obj.nextDouble();
+        int a = obj.nextInt();
         System.out.println("Enter the \"b\" value");
-        double b = obj.nextDouble();
+        int b = obj.nextInt();
         System.out.println("Enter the \"c\" value");
-        double c = obj.nextDouble();
-        double insideroot = b*b-4.0*a*c ;
+        int c = obj.nextInt();
+        int insideroot = b*b-4.0*a*c ;
         
-        if (insideroot > 0.0){
-            double root1 = (-b-Math.pow(insideroot , 0.5))/2.0 * a;
-            double root2 = (-b+Math.pow(insideroot , 0.5))/2.0 * a;
+        if (insideroot > 0){
+            float root1 = (-b-Math.pow(insideroot , 0.5))/2 * a;
+            float root2 = (-b+Math.pow(insideroot , 0.5))/2 * a;
             System.out.println("The root1 is "+root1+"\nThe root2 is "+root2);
         }
         else if (insideroot == 0){
-            double root1 = -b/2.0 * a;
+            float root1 = -b/2 * a;
         }
         else{
             System.out.println("Enter the real value");
